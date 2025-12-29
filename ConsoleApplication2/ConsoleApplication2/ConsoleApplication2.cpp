20db8658f90e95f759d5f40bdfbdaabe04736101
Momchil Buyukliev <113774637+momchil753@users.noreply.github.com>
2025-12-29 20:36:21 +0200

    Update print statement from 'Hello' to 'Goodbye'

--- ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -401,7 +401,7 @@ int main()
     Currency currencies[MAX_CURRENCIES];
     unsigned int currencyCount = 0;
 
-    system("CHCP 1251 > nul");
+    setlocale("LC_ALL", "Bulgarian");
 
     loadFromFile(currencies, currencyCount);
 
@@ -556,3 +556,4 @@ int main()
 // // 4. Use the Error List window to view errors 
 // // 5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project 
 // // 6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
